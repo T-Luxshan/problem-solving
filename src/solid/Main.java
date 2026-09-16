79b11f3144b09579e6e16e30560ff4e2b87d27d4
T-Luxshan <luxshan.thuraisingam@gmail.com>
2026-09-16 09:19:08 +0530

    SOLID files

--- src/solid/Main.java
+++ src/solid/Main.java
@@ -1,3 +1,5 @@
+package solid;
+
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
--- src/solid/RocknPaper.java
+++ src/solid/RocknPaper.java
@@ -1,3 +1,5 @@
+package solid;
+
 import java.util.Random;
 public class RocknPaper {
 
