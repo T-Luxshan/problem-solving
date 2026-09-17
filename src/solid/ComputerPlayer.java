c077530cf7c00f81cb9511a708ec554f1940f3dd
T-Luxshan <luxshan.thuraisingam@gmail.com>
2026-09-17 23:49:01 +0530

    ComputerPlayer init

--- src/solid/ComputerPlayer.java
+++ src/solid/ComputerPlayer.java
@@ -1,6 +1,12 @@
 package solid;
 public class ComputerPlayer implements Player {
 
+    private final GestureProvider gestureProvider;
+
+    public ComputerPlayer(GestureProvider gestureProvider) {
+        this.gestureProvider = gestureProvider;
+    }
+
     @Override
     public Gesture chooseGesture() {
         return null;
