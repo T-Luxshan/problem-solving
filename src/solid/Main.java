0bc29c36880cf0d3012b14d1168e3f7bcbff11be
T-Luxshan <luxshan.thuraisingam@gmail.com>
2026-09-17 23:57:24 +0530

    create main objs

---
+++ src/solid/Main.java
@@ -0,0 +1,26 @@
+package solid;
+
+import solid.rockpaperscissors.RockPaperScissorsGestureProvider;
+import solid.rockpaperscissors.RockPaperScissorsRules;
+
+public class Main {
+    public static void main(String[] args){
+        GestureProvider gestureProvider = new RockPaperScissorsGestureProvider();
+
+        Player humanPlayer = new HumanPlayer();
+
+        Player computerPlayer = new ComputerPlayer(gestureProvider);
+
+        // 3. Create the game rules
+        GameRules gameRules = new RockPaperScissorsRules();
+
+        // 4. Create the game engine
+        GameEngine gameEngine = new GameEngine(humanPlayer, computerPlayer, gameRules);
+
+        // 5. Play one round
+//        GameResult result = gameEngine.playRound();
+
+        // 6. Display the result
+//        System.out.println("Result: " + result);
+    }
+}
