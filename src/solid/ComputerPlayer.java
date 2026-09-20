e79cb3015fd29146e1f4a4c36961d25d36b26563
T-Luxshan <luxshan.thuraisingam@gmail.com>
2026-09-20 23:38:00 +0530

    defined ComputerPlayer with abstraction

--- src/solid/ComputerPlayer.java
+++ src/solid/ComputerPlayer.java
@@ -1,7 +1,12 @@
 package solid;
+
+import java.util.List;
+import java.util.Random;
+
 public class ComputerPlayer implements Player {
 
     private final GestureProvider gestureProvider;
+    private final Random random = new Random();
 
     public ComputerPlayer(GestureProvider gestureProvider) {
         this.gestureProvider = gestureProvider;
@@ -9,6 +14,9 @@ public class ComputerPlayer implements Player {
 
     @Override
     public Gesture chooseGesture() {
-        return null;
+        List<Gesture> gestures = gestureProvider.getGestures();
+        int randomIndex = random.nextInt(gestures.size());
+        System.out.println("Computer choose: "+gestures.get(randomIndex).getType());
+        return gestures.get(randomIndex);
     }
 }
