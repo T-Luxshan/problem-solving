411ae710ab3d49f2527a5288951b82dcef9a26f0
T-Luxshan <luxshan.thuraisingam@gmail.com>
2026-09-20 23:38:21 +0530

    decoupled HumanPlayer

--- src/solid/HumanPlayer.java
+++ src/solid/HumanPlayer.java
@@ -1,30 +1,35 @@
 package solid;
 
-import solid.rockpaperscissors.Paper;
-import solid.rockpaperscissors.Rock;
-import solid.rockpaperscissors.Scissors;
-
+import java.util.List;
 import java.util.Scanner;
 
 public class HumanPlayer implements Player{
     Scanner sc = new Scanner(System.in);
+    public final GestureProvider gestureProvider;
+
+    public HumanPlayer(GestureProvider gestureProvider) {
+        this.gestureProvider = gestureProvider;
+    }
+
     @Override
     public Gesture chooseGesture() {
 
-
-        System.out.println("Choose your gesture:\n1.Rock\n2.Paper\n3.Scissors");
-        System.out.print("Enter choice:");
+        List<Gesture> gestures = gestureProvider.getGestures();
+        printMenu(gestures);
+        System.out.print("Enter your choice:");
         int choice = sc.nextInt();
 
-        while (choice > 3 || choice < 1){
-            System.out.println("Invalid input try again.");
+        while (choice > gestures.size() || choice < 1){
+            System.out.print("Invalid input try again:");
             choice = sc.nextInt();
         }
-        return switch (choice) {
-            case 1 -> new Rock();
-            case 2 -> new Paper();
-            case 3 -> new Scissors();
-            default -> throw new IllegalArgumentException("Invalid choice");
-        };
+
+        return gestures.get(choice-1);
+    }
+
+    public void printMenu(List<Gesture> gestures){
+        for(int i=0; i<gestures.size(); i++){
+            System.out.println(i+1 + "." + gestures.get(i).getType());
+        }
     }
 }
